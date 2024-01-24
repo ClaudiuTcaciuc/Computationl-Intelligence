6ce431002ba7fb1b55be3a02ef444b00b9196a4c
ClaudiuTcaciuc <102698780+ClaudiuTcaciuc@users.noreply.github.com>
2024-01-24 12:43:13 +0100

    work in progress

--- virtual_env/quixo/main.py
+++ virtual_env/quixo/main.py
@@ -1,6 +1,11 @@
 import random
 from game import Game, Move, Player
 
+""" Strategy to implement:
+    - Q-learning
+    - Monte Carlo
+    - Genetic Algorithm
+"""
 
 class RandomPlayer(Player):
     def __init__(self) -> None:
@@ -12,10 +17,28 @@ class RandomPlayer(Player):
         return from_pos, move
 
 
+class MyPlayer(Player):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def make_move(self, game: 'Game') -> tuple[tuple[int, int], Move]:
+        from_pos = (random.randint(0, 4), random.randint(0, 4))
+        move = random.choice([Move.TOP, Move.BOTTOM, Move.LEFT, Move.RIGHT])
+        return from_pos, move
+
+class QLearningPlayer(Player):
+    def __init__(self, alpha: float, gamma: float, epsilon: float) -> None:
+        super().__init__()
+        self._alpha = alpha # learning rate
+        self._gamma = gamma # discount factor
+        self._epsilon = epsilon # exploration rate
+        self._q_table = {} # Q-table to store the Q-values for each state-action pair
+    
+
 if __name__ == '__main__':
     g = Game()
     g.print()
-    player1 = RandomPlayer()
+    player1 = MyPlayer()
     player2 = RandomPlayer()
     winner = g.play(player1, player2)
     g.print()
